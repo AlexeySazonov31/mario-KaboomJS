401062d2b90add534838a5f000c9f3deeb610214
Sazonov Alexey <sazonleha010203@gmail.com>
2022-05-29 16:45:16 +0300

    add

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let app = express();
+
+app.use(express.static("build"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(3000, () => {
+  console.log("running");
+});
