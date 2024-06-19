347f1a81acd524c94ff647cd78bc74ceea07e85a
Aliaksei Sazonau <sazonleha010203@gmail.com>
2024-06-19 14:49:12 +0300

    vercel deploy + favicon

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import path from "path";
 import { fileURLToPath } from "url";
@@ -8,6 +9,7 @@ const __dirname = path.dirname(__filename);
 
 let app = express();
 
+app.use(cors())
 app.use(express.static("build"));
 
 app.get("/", (req, res) => {
