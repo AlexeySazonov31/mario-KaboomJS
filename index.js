68c71c15bf6f9a0f720a550413318cfeff8b8f24
Aliaksei Sazonau <sazonleha010203@gmail.com>
2024-06-22 04:35:53 +0300

    resolve deploy errors

--- index.js
+++ index.js
@@ -20,19 +20,3 @@ app.get("*", function (req, res) {
 app.listen(3000, () => {
   console.log("running");
 });
-
-
-// const express = require('express');
-// const path = require('path');
-
-// const app = express();
-// const port = process.env.PORT || 3000;
-
-// app.use(express.static("build"));
-
-// // sendFile will go here
-// app.get('/', function (req, res) {
-//   res.sendFile(path.join(__dirname, '/index.html'));
-// });
-
-// app.listen(port);
