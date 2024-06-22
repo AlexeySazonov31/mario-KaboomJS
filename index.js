2a3b4beeadd7bde34c5b918aa15050334fc5ab40
Aliaksei Sazonau <sazonleha010203@gmail.com>
2024-06-22 03:53:22 +0300

    trying resolve deploy errors

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
-import express from "express";
+// import express from "express";
+// import path from "path";
 
-import path from "path";
-import { fileURLToPath } from "url";
+// let app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// app.use(express.static("build"));
 
-let app = express();
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, '/index.html'));
+// });
+
+// app.get("*", (req, res) => {
+//   res.redirect('/');
+// })
+
+// app.listen(3000, () => {
+//   console.log("running");
+// });
+
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static("build"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+// sendFile will go here
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.get("*", (req, res) => {
-  res.redirect('/');
-})
-
-app.listen(3000, () => {
-  console.log("running");
-});
+app.listen(port);
