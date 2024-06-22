f9cd430a6ae06633f369469667448a398d33114b
Aliaksei Sazonau <sazonleha010203@gmail.com>
2024-06-22 04:09:49 +0300

    trying resolve deploy errors

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-// import express from "express";
-// import path from "path";
-
-// let app = express();
-
-// app.use(express.static("build"));
-
-// app.get('/', function (req, res) {
-//   res.sendFile(path.join(__dirname, '/index.html'));
-// });
-
-// app.get("*", (req, res) => {
-//   res.redirect('/');
-// })
-
-// app.listen(3000, () => {
-//   console.log("running");
-// });
-
-
 const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.use(express.static("build"));
+app.use(express.static(path.join(__dirname, "/build")));
 
-// sendFile will go here
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.listen(port);
+app.get("*", function (req, res) {
+  res.redirect('/');
+})
+
+app.listen(3000, () => {
+  console.log("running");
+});
+
+
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// app.use(express.static("build"));
+
+// // sendFile will go here
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, '/index.html'));
+// });
+
+// app.listen(port);
